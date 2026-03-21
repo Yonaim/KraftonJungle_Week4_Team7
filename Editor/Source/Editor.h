8ed79789e3e374779dcdcfbfc5b1a64772016220
Yonaim <yonaimyo@gmail.com>
2026-03-21 22:34:57 +0900

    Fix: 인클루드 경로

--- Editor/Source/Editor.h
+++ Editor/Source/Editor.h
@@ -1,43 +1,38 @@
 #pragma once
 
 #include "Core/CoreMinimal.h"
-#include "Renderer/Scene.h"
+#include "Engine/Scene.h"
 
 class FEditor
 {
-public:
+  public:
     /* Default Functions */
     void Create(HWND);
     void Release();
-    
+
     void BeginPlay();
     void Tick(float DeltaTime);
-    
+
     void OnWindowResized(float Width, float Height);
     void SetMainLoopFPS(float FPS) { CurFPS = FPS; }
-    
+
     /* From Panel */
     void CreateNewScene();
     void ClearScene();
-    
-private:
-    
-    
-public:
-    
-    
-private:
+
+  private:
+  public:
+  private:
     /* Panel */
-    
+
     /* Gizmo */
-    
+
     /* Scene */
-    FScene * CurScene = nullptr;
-    
+    FScene* CurScene = nullptr;
+
     /* Properties */
     float WindowWidth = 0.0f;
     float WindowHeight = 0.0f;
-    
-    float CurFPS = 0.0f;    //  Panel에 Display
-    
+
+    float CurFPS = 0.0f; //  Panel에 Display
 };
