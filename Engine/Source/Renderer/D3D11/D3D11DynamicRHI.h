f4e6e8bd7a6245a6fab770962f0cea84a921821a
Yonaim <yonaimyo@gmail.com>
2026-03-21 18:43:20 +0900

    Feat: D3D11 RHI 인터페이스 업데이트
    - Vsync on/off 변수 추가
    - 쉐이더 컴파일 유틸 함수 추가

--- Engine/Source/Renderer/D3D11/D3D11DynamicRHI.h
+++ Engine/Source/Renderer/D3D11/D3D11DynamicRHI.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "D3D11/D3D11Common.h"
-#include "HAL/PlatformTypes.h"
 #include <Windows.h>
+#include <d3dcompiler.h>
+#include "Core/HAL/PlatformTypes.h"
+#include "Renderer/D3D11/D3D11Common.h"
 
 class FD3D11DynamicRHI
 {
@@ -15,13 +16,13 @@ class FD3D11DynamicRHI
 
     bool Resize(int32 InWidth, int32 InHeight);
 
-    ID3D11Device        *GetDevice() const { return Device.Get(); }
-    ID3D11DeviceContext *GetDeviceContext() const { return DeviceContext.Get(); }
-    IDXGISwapChain      *GetSwapChain() const { return SwapChain.Get(); }
+    ID3D11Device*        GetDevice() const { return Device.Get(); }
+    ID3D11DeviceContext* GetDeviceContext() const { return DeviceContext.Get(); }
+    IDXGISwapChain*      GetSwapChain() const { return SwapChain.Get(); }
 
-    ID3D11RenderTargetView *GetBackBufferRTV() const { return BackBufferRTV.Get(); }
-    ID3D11Texture2D        *GetDepthStencilBuffer() const { return DepthStencilBuffer.Get(); }
-    ID3D11DepthStencilView *GetDepthStencilView() const { return DepthStencilView.Get(); }
+    ID3D11RenderTargetView* GetBackBufferRTV() const { return BackBufferRTV.Get(); }
+    ID3D11Texture2D*        GetDepthStencilBuffer() const { return DepthStencilBuffer.Get(); }
+    ID3D11DepthStencilView* GetDepthStencilView() const { return DepthStencilView.Get(); }
 
     D3D11_VIEWPORT GetViewport() const { return Viewport; }
 
@@ -31,6 +32,24 @@ class FD3D11DynamicRHI
     void SetViewport(int32 InWidth, int32 InHeight);
     void SetDefaultRenderTargets();
     void Clear(const FLOAT InClearColor[4], float InDepth = 1.0f, uint8 InStencil = 0);
+
+    void SetVSyncEnabled(bool bInVSyncEnabled) { bVSyncEnabled = bInVSyncEnabled; }
+    bool IsVSyncEnabled() const { return bVSyncEnabled; }
+
+  public:
+    bool CompileShaderFromFile(const wchar_t* InFilePath, const char* InEntryPoint,
+                               const char* InTarget, ID3DBlob** OutShaderBlob) const;
+
+    bool CreateVertexShaderAndInputLayout(const wchar_t* InFilePath, const char* InEntryPoint,
+                                          const D3D11_INPUT_ELEMENT_DESC* InInputElements,
+                                          uint32                          InInputElementCount,
+                                          ID3D11VertexShader**            OutVertexShader,
+                                          ID3D11InputLayout**             OutInputLayout) const;
+
+    bool CreatePixelShader(const wchar_t* InFilePath, const char* InEntryPoint,
+                           ID3D11PixelShader** OutPixelShader) const;
+
+    bool CreateConstantBuffer(uint32 InByteWidth, ID3D11Buffer** OutConstantBuffer) const;
 
   private:
     bool CreateDeviceAndSwapChain(HWND InWindowHandle);
@@ -43,6 +62,8 @@ class FD3D11DynamicRHI
 
     int32 ViewportWidth = 0;
     int32 ViewportHeight = 0;
+
+    bool bVSyncEnabled = true;
 
     TComPtr<ID3D11Device>        Device;
     TComPtr<ID3D11DeviceContext> DeviceContext;
