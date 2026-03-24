9e95a188f4a0a0350c2a08b7e9166a88a4add355
an_yan_yang <yanggogi1423@gmail.com>
2026-03-25 01:07:03 +0900

    Feat : SpriteTextureActor, SubUVTextureActor, SpriteAnimationActor

---
+++ Engine/Source/Engine/Game/SpriteTextureActor.cpp
@@ -0,0 +1,68 @@
+﻿#include "Core/CoreMinimal.h"
+#include "SpriteTextureActor.h"
+
+#include "Engine/Component/Core/PrimitiveComponent.h"
+#include "Engine/Component/Sprite/SpriteComponent.h"
+
+namespace
+{
+    FColor ToColor(const FVector4& InColor)
+    {
+        return FColor(InColor.X, InColor.Y, InColor.Z, InColor.W);
+    }
+} // namespace
+
+ASpriteTextureActor::ASpriteTextureActor()
+{
+    auto* SpriteComponent = new Engine::Component::USpriteComponent();
+    SpriteComponent->SetColor({ 0.8f,0.8f,0.8f,1.f });
+    AddOwnedComponent(SpriteComponent, true);
+
+    Name = "ConeActor";
+}
+
+Engine::Component::USpriteComponent* ASpriteTextureActor::GetSpriteTextureComponent() const
+{
+    return Cast<Engine::Component::USpriteComponent>(RootComponent);
+}
+
+bool ASpriteTextureActor::IsRenderable() const { return GetPrimitiveComponent() != nullptr; }
+
+bool ASpriteTextureActor::IsSelected() const
+{
+    if (RootComponent == nullptr)
+    {
+        return false;
+    }
+
+    return RootComponent->IsSelected();
+}
+
+FColor ASpriteTextureActor::GetColor() const
+{
+    if (const auto* PrimitiveComponent = GetPrimitiveComponent())
+    {
+        return PrimitiveComponent->GetColor();
+    }
+
+    return FColor::White();
+}
+
+EBasicMeshType ASpriteTextureActor::GetMeshType() const
+{
+    if (const auto* ConeComponent = GetPrimitiveComponent())
+    {
+        return ConeComponent->GetBasicMeshType();
+    }
+
+    return AActor::GetMeshType();
+}
+
+uint32 ASpriteTextureActor::GetObjectId() const { return UUID; }
+
+Engine::Component::UPrimitiveComponent* ASpriteTextureActor::GetPrimitiveComponent() const
+{
+    return Cast<Engine::Component::UPrimitiveComponent>(RootComponent);
+}
+
+REGISTER_CLASS(, ASpriteTextureActor)
---
+++ Engine/Source/Engine/Game/SpriteTextureActor.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Actor.h"
+
+namespace Engine::Component
+{
+    class UPrimitiveComponent;
+    class USpriteComponent;
+}
+
+class ENGINE_API ASpriteTextureActor : public AActor
+{
+    DECLARE_RTTI(AConeActor, AActor)
+public:
+    ASpriteTextureActor();
+    ~ASpriteTextureActor() override = default;
+    
+    Engine::Component::USpriteComponent * GetSpriteTextureComponent() const;
+    
+    bool           IsRenderable() const override;
+    bool           IsSelected() const override;
+    FColor         GetColor() const override;
+    EBasicMeshType GetMeshType() const override;
+    uint32         GetObjectId() const override;
+    
+private:
+    Engine::Component::UPrimitiveComponent* GetPrimitiveComponent() const;
+};
