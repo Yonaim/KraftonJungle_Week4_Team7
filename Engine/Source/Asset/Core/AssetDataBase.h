1ff1ba3883c4fc1d6f7e4360d227d7aba841a759
Yonaim <yonaimyo@gmail.com>
2026-03-29 17:41:01 +0900

    Feat: Asset Data Base

---
+++ Engine/Source/Asset/Core/AssetDataBase.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Asset/Core/Types.h"
+
+// TODO: 삭제 고려
+
+// enum class EAssetType : uint8
+// {
+//     Unknown = 0,
+//     Texture,
+//     Material,
+//     StaticMesh,
+//     SubUVAtlas,
+//     FontAtlas,
+// };
+
+// struct IAssetData
+// {
+//     virtual ~IAssetData() = default;
+//     virtual EAssetType GetAssetType() const = 0;
+//     virtual bool       IsValid() const = 0;
+//     virtual void       Reset() = 0;
+// };
