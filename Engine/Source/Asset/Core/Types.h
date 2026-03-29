bcf4299a4ce8d67cacebeef035e4dba920086651
Yonaim <yonaimyo@gmail.com>
2026-03-29 17:41:43 +0900

    Feat: VertexPNCT

---
+++ Engine/Source/Asset/Core/Types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Core/CoreMinimal.h"
+
+// TODO: 세분화 (특정 필드만 있는 경우)
+
+struct FVertexPNCT
+{
+    FVector  Position;
+    FVector  Normal;
+    FVector  Color;
+    FVector2 UV;
+};
