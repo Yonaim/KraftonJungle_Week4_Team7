742d9beae4098a44eaa0a1e6b7625b0dd1801b7d
Yonaim <yonaimyo@gmail.com>
2026-03-30 12:57:13 +0900

    Feat: Key 체계 변경

--- Engine/Source/Asset/Cache/AssetCache.h
+++ Engine/Source/Asset/Cache/AssetCache.h
@@ -1,52 +1,30 @@
 #pragma once
 
 #include <memory>
-#include <type_traits>
 
-#include "Asset/Core/AssetDataBase.h"
-#include "Asset/Cache/AssetKey.h"
+#include "Core/CoreMinimal.h"
 
 // ============================== Generic Cache ===============================
 
 template <typename TData> class TAssetCache
 {
   public:
-    std::shared_ptr<TData> Find(const FAssetKey& Key) const
+    std::shared_ptr<TData> Find(const TKey& Key) const
     {
         auto It = Records.find(Key);
         return It != Records.end() ? It->second : nullptr;
     }
 
-    template <typename TKey> std::shared_ptr<TData> Find(const TKey& Key) const
-    {
-        static_assert(std::is_base_of_v<FAssetKey, TKey>, "TKey must derive from FAssetKey.");
-        return Find(static_cast<const FAssetKey&>(Key));
-    }
-
-    void Insert(const FAssetKey& Key, const std::shared_ptr<TData>& Data) { Records[Key] = Data; }
-
-    template <typename TKey> void Insert(const TKey& Key, const std::shared_ptr<TData>& Data)
-    {
-        static_assert(std::is_base_of_v<FAssetKey, TKey>, "TKey must derive from FAssetKey.");
-        Insert(static_cast<const FAssetKey&>(Key), Data);
-    }
-
-    void Remove(const FAssetKey& Key) { Records.erase(Key); }
-
-    template <typename TKey> void Remove(const TKey& Key)
-    {
-        static_assert(std::is_base_of_v<FAssetKey, TKey>, "TKey must derive from FAssetKey.");
-        Remove(static_cast<const FAssetKey&>(Key));
-    }
-
+    void Insert(const TKey& Key, const std::shared_ptr<TData>& Data) { Records[Key] = Data; }
+    void Remove(const TKey& Key) { Records.erase(Key); }
     void Clear() { Records.clear(); }
 
   private:
-    TMap<FAssetKey, std::shared_ptr<TData>> Records;
+    TMap<TKey, std::shared_ptr<TData>> Records;
 };
 
-// =============================== Type Aliases ===============================
+template <typename TKey, typename TIntermediate>
+using TIntermediateCache = TAssetCache<TKey, TIntermediate>;
 
-template <typename TIntermediate> using TIntermediateCache = TAssetCache<TIntermediate>;
-
-template <typename TCooked> using TCookedCache = TAssetCache<TCooked>;
+template <typename TKey, typename TCooked>
+using TCookedCache = TAssetCache<TKey, TCooked>;
--- Engine/Source/Asset/Cache/AssetCacheTraits.h
+++ Engine/Source/Asset/Cache/AssetCacheTraits.h
@@ -1,16 +1,17 @@
 #pragma once
 
+#include "Asset/Cache/AssetKey.h"
 #include "Asset/Cache/AssetTags.h"
-#include "Asset/Intermediate/IntermediateTextureData.h"
-#include "Asset/Intermediate/IntermediateMaterialData.h"
-#include "Asset/Intermediate/IntermediateStaticMeshData.h"
-#include "Asset/Intermediate/IntermediateSubUVAtlasData.h"
-#include "Asset/Intermediate/IntermediateFontAtlasData.h"
-#include "Asset/Cooked/TextureCookedData.h"
+#include "Asset/Cooked/FontAtlasCookedData.h"
 #include "Asset/Cooked/MaterialCookedData.h"
 #include "Asset/Cooked/StaticMeshCookedData.h"
 #include "Asset/Cooked/SubUVAtlasCookedData.h"
-#include "Asset/Cooked/FontAtlasCookedData.h"
+#include "Asset/Cooked/TextureCookedData.h"
+#include "Asset/Intermediate/IntermediateFontAtlasData.h"
+#include "Asset/Intermediate/IntermediateMaterialData.h"
+#include "Asset/Intermediate/IntermediateStaticMeshData.h"
+#include "Asset/Intermediate/IntermediateSubUVAtlasData.h"
+#include "Asset/Intermediate/IntermediateTextureData.h"
 
 namespace Asset
 {
@@ -19,38 +20,53 @@ template <typename TTag> struct TAssetCacheTraits;
 
 template <> struct TAssetCacheTraits<FTextureAssetTag>
 {
+    using SourceKey = FTextureSourceKey;
+    using IntermediateKey = FTextureIntermediateKey;
+    using CookedKey = FTextureCookedKey;
     using IntermediateType = FIntermediateTextureData;
     using CookedType = FTextureCookedData;
 };
 
 template <> struct TAssetCacheTraits<FMaterialAssetTag>
 {
+    using SourceKey = FMaterialSourceKey;
+    using IntermediateKey = FMaterialIntermediateKey;
+    using CookedKey = FMaterialCookedKey;
     using IntermediateType = FIntermediateMaterialData;
     using CookedType = FMaterialCookedData;
 };
 
 template <> struct TAssetCacheTraits<FStaticMeshAssetTag>
 {
+    using SourceKey = FStaticMeshSourceKey;
+    using IntermediateKey = FStaticMeshIntermediateKey;
+    using CookedKey = FStaticMeshCookedKey;
     using IntermediateType = FIntermediateStaticMeshData;
     using CookedType = FStaticMeshCookedData;
 };
 
 template <> struct TAssetCacheTraits<FSubUVAtlasAssetTag>
 {
+    using SourceKey = FSubUVAtlasSourceKey;
+    using IntermediateKey = FSubUVAtlasIntermediateKey;
+    using CookedKey = FSubUVAtlasCookedKey;
     using IntermediateType = FIntermediateSubUVAtlasData;
     using CookedType = FSubUVAtlasCookedData;
 };
 
 template <> struct TAssetCacheTraits<FFontAtlasAssetTag>
 {
+    using SourceKey = FFontAtlasSourceKey;
+    using IntermediateKey = FFontAtlasIntermediateKey;
+    using CookedKey = FFontAtlasCookedKey;
     using IntermediateType = FIntermediateFontAtlasData;
     using CookedType = FFontAtlasCookedData;
 };
 
-template <typename TTag>
-using TIntermediateTypeOf = typename TAssetCacheTraits<TTag>::IntermediateType;
-
-template <typename TTag>
-using TCookedTypeOf = typename TAssetCacheTraits<TTag>::CookedType;
+template <typename TTag> using TSourceKeyOf = typename TAssetCacheTraits<TTag>::SourceKey;
+template <typename TTag> using TIntermediateKeyOf = typename TAssetCacheTraits<TTag>::IntermediateKey;
+template <typename TTag> using TCookedKeyOf = typename TAssetCacheTraits<TTag>::CookedKey;
+template <typename TTag> using TIntermediateTypeOf = typename TAssetCacheTraits<TTag>::IntermediateType;
+template <typename TTag> using TCookedTypeOf = typename TAssetCacheTraits<TTag>::CookedType;
 
 } // namespace Asset
